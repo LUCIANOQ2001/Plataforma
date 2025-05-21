e4ab24097d46808bea3ecb0511fc6d7979adfb4d
LUCIANO QUIROZ GONZALES <136769391+LUCIANOQ2001@users.noreply.github.com>
2025-05-20 23:29:11 -0500

    Cambios del 20 de Mayo a las tres de la tarde

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 	require_once "./core/configGeneral.php";
 	require_once "./controllers/viewsController.php";
 
+	session_start();
+	
 	$ViewTemplate=new viewsController();
 	$ViewTemplate->get_template();
 	
+	?>
