70cdd2f84ed8e3cdb0032bb616d69ac6f8ea3f14
LUCIANO QUIROZ GONZALES <136769391+LUCIANOQ2001@users.noreply.github.com>
2025-05-30 12:55:03 -0500

    CAMBIOS DEL 30 DE MAYO :D

--- views/inc/sidebar.php
+++ views/inc/sidebar.php
@@ -19,16 +19,16 @@ if (!in_array($_SESSION['userType'] ?? '', ['Administrador','Docente','Estudiant
     <div class="full-box dashboard-sideBar-UserInfo">
       <figure class="full-box">
         <img src="<?= SERVERURL ?>views/assets/img/logo.png" alt="UserIcon">
-        <figcaption class="text-center text-titles"><?= htmlspecialchars($_SESSION['userName']) ?></figcaption>
+        <figcaption class="text-center text-titles">
+          <?= htmlspecialchars($_SESSION['displayName'] ?? $_SESSION['userName']) ?>
+        </figcaption>
       </figure>
       <ul class="full-box list-unstyled text-center">
-        <?php if (in_array($_SESSION['userType'], ['Administrador','Docente'])): ?>
         <li>
           <a href="<?= SERVERURL ?>account/<?= $_SESSION['userKey'] ?>/">
             <i class="zmdi zmdi-settings"></i>
           </a>
         </li>
-        <?php endif; ?>
         <li>
           <a href="#!" class="btnFormsAjax" data-action="logout" data-id="form-logout">
             <i class="zmdi zmdi-power"></i>
@@ -41,57 +41,43 @@ if (!in_array($_SESSION['userType'] ?? '', ['Administrador','Docente','Estudiant
     </div>
 
     <ul class="list-unstyled full-box dashboard-sideBar-Menu">
-
     <?php if ($_SESSION['userType'] === "Administrador"): ?>
-
       <!-- Menú Administrador -->
-      <li>
-        <a href="<?= SERVERURL ?>dashboard/">
-          <i class="zmdi zmdi-view-dashboard zmdi-hc-fw"></i> Inicio
-        </a>
-      </li>
-
+      <li><a href="<?= SERVERURL ?>dashboard/"><i class="zmdi zmdi-view-dashboard"></i> Inicio</a></li>
       <li>
         <a href="#!" class="btn-sideBar-SubMenu">
-          <i class="zmdi zmdi-account zmdi-hc-fw"></i> Docentes
-          <i class="zmdi zmdi-caret-down pull-right"></i>
+          <i class="zmdi zmdi-account"></i> Docentes <i class="zmdi zmdi-caret-down pull-right"></i>
         </a>
-        <ul class="list-unstyled full-box submenu">
+        <ul class="submenu list-unstyled full-box">
           <li><a href="<?= SERVERURL ?>admin/">Nuevo Docente</a></li>
           <li><a href="<?= SERVERURL ?>adminlist/">Lista de Docentes</a></li>
-          <li><a href="<?= SERVERURL ?>consultaslist/">Historial de Consultas</a></li>
+          <li><a href="<?= SERVERURL ?>consultaslist/">Consultas</a></li>
         </ul>
       </li>
-
       <li>
         <a href="#!" class="btn-sideBar-SubMenu">
-          <i class="zmdi zmdi-face zmdi-hc-fw"></i> Estudiantes
-          <i class="zmdi zmdi-caret-down pull-right"></i>
+          <i class="zmdi zmdi-face"></i> Estudiantes <i class="zmdi zmdi-caret-down pull-right"></i>
         </a>
-        <ul class="list-unstyled full-box submenu">
+        <ul class="submenu list-unstyled full-box">
           <li><a href="<?= SERVERURL ?>student/">Nuevo Estudiante</a></li>
-          <li><a href="<?= SERVERURL ?>studentlist/">Listado de Estudiantes</a></li>
-          <li><a href="<?= SERVERURL ?>asistencia/">Registro de Asistencias</a></li>
+          <li><a href="<?= SERVERURL ?>studentlist/">Lista de Estudiantes</a></li>
+          <li><a href="<?= SERVERURL ?>asistencia/">Asistencias</a></li>
         </ul>
       </li>
-
       <li>
         <a href="#!" class="btn-sideBar-SubMenu">
-          <i class="zmdi zmdi-videocam zmdi-hc-fw"></i> Clases
-          <i class="zmdi zmdi-caret-down pull-right"></i>
+          <i class="zmdi zmdi-videocam"></i> Clases <i class="zmdi zmdi-caret-down pull-right"></i>
         </a>
-        <ul class="list-unstyled full-box submenu">
+        <ul class="submenu list-unstyled full-box">
           <li><a href="<?= SERVERURL ?>class/">Nueva Clase</a></li>
           <li><a href="<?= SERVERURL ?>classlist/">Lista de Clases</a></li>
         </ul>
       </li>
-
       <li>
         <a href="#!" class="btn-sideBar-SubMenu">
-          <i class="zmdi zmdi-book zmdi-hc-fw"></i> Cursos
-          <i class="zmdi zmdi-caret-down pull-right"></i>
+          <i class="zmdi zmdi-book"></i> Cursos <i class="zmdi zmdi-caret-down pull-right"></i>
         </a>
-        <ul class="list-unstyled full-box submenu">
+        <ul class="submenu list-unstyled full-box">
           <li><a href="<?= SERVERURL ?>curso/">Nuevo Curso</a></li>
           <li><a href="<?= SERVERURL ?>cursolist/">Lista de Cursos</a></li>
         </ul>
@@ -101,48 +87,41 @@ if (!in_array($_SESSION['userType'] ?? '', ['Administrador','Docente','Estudiant
 
       <!-- Menú Docente -->
       <li>
-        <a href="<?= SERVERURL ?>dashboard/">
-          <i class="zmdi zmdi-view-dashboard zmdi-hc-fw"></i> Inicio
+        <a href="<?= SERVERURL ?>studentinfo/<?= $_SESSION['userKey'] ?>/">
+          <i class="zmdi zmdi-account-box"></i> Mis datos
         </a>
       </li>
-
+      <li><a href="<?= SERVERURL ?>dashboard/"><i class="zmdi zmdi-view-dashboard"></i> Inicio</a></li>
       <li>
         <a href="#!" class="btn-sideBar-SubMenu">
-          <i class="zmdi zmdi-comment-text zmdi-hc-fw"></i> Consultas
-          <i class="zmdi zmdi-caret-down pull-right"></i>
+          <i class="zmdi zmdi-comment-text"></i> Consultas <i class="zmdi zmdi-caret-down pull-right"></i>
         </a>
-        <ul class="list-unstyled full-box submenu">
+        <ul class="submenu list-unstyled full-box">
           <li><a href="<?= SERVERURL ?>consultaslist/">Historial de Consultas</a></li>
         </ul>
       </li>
-
       <li>
         <a href="#!" class="btn-sideBar-SubMenu">
-          <i class="zmdi zmdi-face zmdi-hc-fw"></i> Estudiantes
-          <i class="zmdi zmdi-caret-down pull-right"></i>
+          <i class="zmdi zmdi-face"></i> Estudiantes <i class="zmdi zmdi-caret-down pull-right"></i>
         </a>
-        <ul class="list-unstyled full-box submenu">
-          <li><a href="<?= SERVERURL ?>studentlist/">Listado de Estudiantes</a></li>
+        <ul class="submenu list-unstyled full-box">
+          <li><a href="<?= SERVERURL ?>studentlist/">Lista de Estudiantes</a></li>
         </ul>
       </li>
-
       <li>
         <a href="#!" class="btn-sideBar-SubMenu">
-          <i class="zmdi zmdi-videocam zmdi-hc-fw"></i> Clases
-          <i class="zmdi zmdi-caret-down pull-right"></i>
+          <i class="zmdi zmdi-videocam"></i> Clases <i class="zmdi zmdi-caret-down pull-right"></i>
         </a>
-        <ul class="list-unstyled full-box submenu">
+        <ul class="submenu list-unstyled full-box">
           <li><a href="<?= SERVERURL ?>class/">Nueva Clase</a></li>
           <li><a href="<?= SERVERURL ?>classlist/">Lista de Clases</a></li>
         </ul>
       </li>
-
       <li>
         <a href="#!" class="btn-sideBar-SubMenu">
-          <i class="zmdi zmdi-book zmdi-hc-fw"></i> Cursos
-          <i class="zmdi zmdi-caret-down pull-right"></i>
+          <i class="zmdi zmdi-book"></i> Cursos <i class="zmdi zmdi-caret-down pull-right"></i>
         </a>
-        <ul class="list-unstyled full-box submenu">
+        <ul class="submenu list-unstyled full-box">
           <li><a href="<?= SERVERURL ?>miscursos/">Mis Cursos</a></li>
         </ul>
       </li>
@@ -151,46 +130,30 @@ if (!in_array($_SESSION['userType'] ?? '', ['Administrador','Docente','Estudiant
 
       <!-- Menú Estudiante -->
       <li>
-        <a href="<?= SERVERURL ?>home/">
-          <i class="zmdi zmdi-view-dashboard zmdi-hc-fw"></i> Inicio
+        <a href="<?= SERVERURL ?>studentinfo/<?= $_SESSION['userKey'] ?>/">
+          <i class="zmdi zmdi-account-box"></i> Mis datos
         </a>
       </li>
-
-      <!-- Opción A: Anuncios recientes y Material de curso -->
-      <li>
-        <a href="<?= SERVERURL ?>anuncio/">
-          <i class="zmdi zmdi-notifications"></i> Anuncios recientes
-        </a>
-      </li>
-      <li>
-        <a href="<?php echo SERVERURL; ?>materialcurso/<?php echo $c['id']; ?>/">
-          <i class="zmdi zmdi-collection-text zmdi-hc-fw"></i> Material de curso
-        </a>
-      </li>
-
+      <li><a href="<?= SERVERURL ?>home/"><i class="zmdi zmdi-view-dashboard"></i> Inicio</a></li>
+      <li><a href="<?= SERVERURL ?>anuncio/"><i class="zmdi zmdi-notifications"></i> Anuncios recientes</a></li>
       <li>
         <a href="#!" class="btn-sideBar-SubMenu">
-          <i class="zmdi zmdi-book zmdi-hc-fw"></i> Cursos
-          <i class="zmdi zmdi-caret-down pull-right"></i>
+          <i class="zmdi zmdi-book"></i> Cursos <i class="zmdi zmdi-caret-down pull-right"></i>
         </a>
-        <ul class="list-unstyled full-box submenu">
+        <ul class="submenu list-unstyled full-box">
           <li><a href="<?= SERVERURL ?>miscursos/">Mis Cursos</a></li>
         </ul>
       </li>
-
       <li>
         <a href="#!" class="btn-sideBar-SubMenu">
-          <i class="zmdi zmdi-comment-text zmdi-hc-fw"></i> Consultas Generales
-          <i class="zmdi zmdi-caret-down pull-right"></i>
+          <i class="zmdi zmdi-comment-text"></i> Consultas <i class="zmdi zmdi-caret-down pull-right"></i>
         </a>
-        <ul class="list-unstyled full-box submenu">
+        <ul class="submenu list-unstyled full-box">
           <li><a href="<?= SERVERURL ?>consultas/">Nueva Consulta</a></li>
-          <li><a href="<?= SERVERURL ?>consultaslist/">Historial de Consultas</a></li>
         </ul>
       </li>
 
     <?php endif; ?>
-
     </ul>
   </div>
 </section>
@@ -200,15 +163,9 @@ if (!in_array($_SESSION['userType'] ?? '', ['Administrador','Docente','Estudiant
     document.querySelectorAll(".btn-sideBar-SubMenu").forEach(btn => {
       btn.addEventListener("click", function(e){
         e.preventDefault();
-        // cerramos todos los submenus
-        document.querySelectorAll(".submenu").forEach(menu => {
-          if (menu !== btn.nextElementSibling) {
-            menu.style.display = "none";
-          }
-        });
-        // alternamos el submenu actual
-        let sibling = btn.nextElementSibling;
-        sibling.style.display = sibling.style.display === "block" ? "none" : "block";
+        document.querySelectorAll(".submenu").forEach(menu=>menu.style.display="none");
+        let sib = btn.nextElementSibling;
+        sib.style.display = sib.style.display==="block"?"none":"block";
       });
     });
   });
